28c1860ec90f932c0359eba56b02c57868280e4e
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-11-15 17:56:13 +0900

    feat: wrap Stack component with ActionSheetProvider for improved action sheet handling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Stack } from 'expo-router';
 
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function RootLayout() {
 	return (
 		<GestureHandlerRootView>
-			<Stack
-				screenOptions={{
-					headerShown: false,
-				}}
-			>
-				<Stack.Screen name="index" />
-			</Stack>
+			<ActionSheetProvider>
+				<Stack
+					screenOptions={{
+						headerShown: false,
+					}}
+				>
+					<Stack.Screen name="index" />
+				</Stack>
+			</ActionSheetProvider>
 		</GestureHandlerRootView>
 	);
 }
